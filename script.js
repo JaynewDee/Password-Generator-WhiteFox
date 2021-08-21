69bd0dce0495f3fbea1581488b422b110c141c49
NuminousBlue <jdiehl2236@gmail.com>
2021-08-21 16:13:25 -0500

    arrays and generate function

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Arrays for potential password characters
+var lowerCase = ["abcdefghijklmnopqrstuvwxyz"];
+var upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var numbers = ["123456789"];
+var specChars = ["!@#$%&*()"]
+
+
+
 function generatePassword(){
   // WHEN prompted for the length of the password
-  var length = prompt("Enter password length")
-  // THEN I choose a length of at least 8 characters and no more than 128 characters
+  var length = prompt("Enter password length");
+    // THEN I choose a length of at least 8 characters and no more than 128 characters
+  if (length > 8 && length < 128) {
+       var lowerCaseOpt = confirm("Include lowercase letters?");
+       var upperCaseOpt = confirm("Include uppercase letters?");
+       var numbersOpt = confirm("Include numbers?");
+       var specCharsOpt = confirm("Include special characters?");
+  }
+     else if (length < 8 && length > 128){
+            return alert("Invalid.  Please choose a password length greater than 8 characters and fewer than 128 characters.");
+       };
+
   console.log(parseInt(length))
-  console.log(typeof parseInt(length))
+  console.log(typeof parseInt(length)) 
   // WHEN asked for character types to include in the password
   // THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
   // WHEN I answer each prompt
